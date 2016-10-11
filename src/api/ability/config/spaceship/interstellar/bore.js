a70c8f413e3e14b6cb5c92cacce2a4bc2cd809f3
Greg <gregmichaeldavies@gmail.com>
2016-10-11 12:09:53 +0100

    Create bore.js

---
+++ src/api/ability/config/spaceship/interstellar/bore.js
@@ -0,0 +1,3 @@
+// Mystery sprite uses this to attack an intelligence officer's interest.
+// Intelligence officer can resist with patience attribute, which will be based on some skill.
+// Interest is a shared capacitor.
