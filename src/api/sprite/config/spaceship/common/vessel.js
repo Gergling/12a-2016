2bdf2bd9fbccb55a3b57044e9ffb75358dbae3dd
Greg <gregmichaeldavies@gmail.com>
2016-10-12 14:59:43 +0100

    Update vessel.js

--- src/api/sprite/config/spaceship/common/vessel.js
+++ src/api/sprite/config/spaceship/common/vessel.js
@@ -1,9 +1,17 @@
 // Attributes:
 // - Visibility
 // - Defense
-// - Attack
+// - Engine
+// - Reactor
 
 // Capacitors:
 // - Defense
-// - Engines
-// - Weapons
+// - Speed
+// -- Maximum depends on engine hardware
+// -- Moving reduces current
+// -- Current is fully replenished each round
+// - Energy
+// -- Maximum depends on reactor hardware and engineering skills
+// -- Current is reduced by certain abilities, depending on certain engineering abilities
+// -- Current is replenished each round depending on power from reactor hardware, and abilities depending on engineering skills
+// - Weapons, similar to above but for ship weapon skills
