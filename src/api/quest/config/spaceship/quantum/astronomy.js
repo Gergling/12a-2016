69306cd4628c2d644885b1df98f50f4631cce506
Greg <gregmichaeldavies@gmail.com>
2016-10-12 16:27:20 +0100

    Update astronomy.js

--- src/api/quest/config/spaceship/quantum/astronomy.js
+++ src/api/quest/config/spaceship/quantum/astronomy.js
@@ -6,3 +6,15 @@
 // Capacitors:
 // Interest: When you run out of interest, you can no longer scan.
 // Processing power: Used on all tool-based abilities.
+
+// This quest is spawned so that the observer can find something.
+// This might be to track or to buff for navigation or other reason.
+// Need to come up with cases why astronomy is happening.
+
+module.exports = {
+    sprites: [
+        'observer', // The number of these available depends on ship hardware.
+        // All observers have a shared interest capacitor.
+        'mystery', // Must be configured to spawn something specific on death
+    ]
+};
