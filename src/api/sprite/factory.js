a1a3791890b9734e48a31bff97343d3c81dc0be2
gergling <gregmichaeldavies@gmail.com>
2016-10-14 19:09:54 +0100

    Factory module will need these functions.

--- src/api/sprite/factory.js
+++ src/api/sprite/factory.js
@@ -2,5 +2,11 @@
 // Each capacitor includes a maximum, current and resistance.
 
 function Sprite() {
-
+    // Will include a list of abilities
 }
+
+function instantiate() {
+    
+}
+
+module.exports = instantiate;
