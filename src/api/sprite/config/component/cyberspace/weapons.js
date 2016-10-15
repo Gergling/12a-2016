5d6095a9d94562b2d838499d01cc2e97d014aff6
gergling <gregmichaeldavies@gmail.com>
2016-10-15 21:20:36 +0100

    Added cyberspace weapons sprite.

---
+++ src/api/sprite/config/component/cyberspace/weapons.js
@@ -0,0 +1,3 @@
+// This is this cyberspace prescence of a physical weapon system.
+// It has minimal defense in cyberspace on it's own, relying primarily on the cyberspace defenses.
+// It also cannot move.
