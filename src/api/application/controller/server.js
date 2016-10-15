e364b2cdd8ae312e0b7433d65fb7d5ac46044c5c
gergling <gregmichaeldavies@gmail.com>
2016-10-15 21:20:05 +0100

    Included index.html file.

--- src/api/application/controller/server.js
+++ src/api/application/controller/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
+app.use(express.static('./src/client/'));
 app.use(express.static('./dist/'));
 
 app.listen(port);
