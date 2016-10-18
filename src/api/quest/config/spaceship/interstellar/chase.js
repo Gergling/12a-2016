a2efadbd9c1a0e093cff922533516e95d8d00a5b
Greg <gregmichaeldavies@gmail.com>
2016-10-18 15:39:21 +0100

    Create chase.js

---
+++ src/api/quest/config/spaceship/interstellar/chase.js
@@ -0,0 +1,4 @@
+// Sprites always face the same way.
+// The background scrolls the stars past.
+// The idea is to gain on another spaceship.
+// Presumably you'd avoid gravity wells and try to use spatial anomalies in your favour.
