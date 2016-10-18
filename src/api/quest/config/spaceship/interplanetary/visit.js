45e7d84599171a31e6031dd5ade38de880467a94
Greg <gregmichaeldavies@gmail.com>
2016-10-18 14:26:57 +0100

    Create visit.js

---
+++ src/api/quest/config/spaceship/interplanetary/visit.js
@@ -0,0 +1,4 @@
+// Your ship moves between planets in a system.
+// They will try to charge you for docking unless you can find free docking.
+// Don't trust the mechanics unless you can keep an eye on them or the place is solidly reputable.
+// Needs more ways to generate drama.
