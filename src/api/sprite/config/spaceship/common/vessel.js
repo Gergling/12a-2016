7b26a6d6c9ddfc108d2c956381550cf8189643e2
Greg <gregmichaeldavies@gmail.com>
2016-10-24 17:55:56 +0100

    Update vessel.js

--- src/api/sprite/config/spaceship/common/vessel.js
+++ src/api/sprite/config/spaceship/common/vessel.js
@@ -15,3 +15,21 @@
 // -- Current is reduced by certain abilities, depending on certain engineering abilities
 // -- Current is replenished each round depending on power from reactor hardware, and abilities depending on engineering skills
 // - Weapons, similar to above but for ship weapon skills
+
+module.exports = {
+    attribute: {
+        // Defenses
+    },
+    capacitor: {
+        // Defenses
+        ams: {
+            
+        },
+        speed: {
+            max: function () {
+                // Engine, buffs
+            },
+            current: 'full',
+        }
+    }
+};
