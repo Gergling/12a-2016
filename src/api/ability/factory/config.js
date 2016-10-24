ca11becb874d006078701c9063f1576d31cc55ce
Greg <gregmichaeldavies@gmail.com>
2016-10-24 11:08:24 +0100

    Update config.js

--- src/api/ability/factory/config.js
+++ src/api/ability/factory/config.js
@@ -1,7 +1,16 @@
 function Ability() {
     var data = {
-        response: function () {} // Required
+        name: '',
+        label: '',
+        range: {
+            minimum: 0,
+            maximum: 0
+        },
+        cooldown: 0, // The number of turns which goes by before the ability becomes available again.
+        cost: {} // This is a pairing of capacitor name against value.
     }
+    
+    // Effect might be too complicated to boil down to an object except for API purposes.
 }
 
 function instantiate() {
