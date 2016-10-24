b64fe01f7152c40a76c82d7378fb9998912a70ed
Greg <gregmichaeldavies@gmail.com>
2016-10-24 16:08:14 +0100

    Update factory.js

--- src/api/sprite/factory.js
+++ src/api/sprite/factory.js
@@ -2,7 +2,10 @@
 // Each capacitor includes a maximum, current and resistance.
 
 function Sprite() {
-    // Will include a list of abilities
+    // Will include a list of abilities and some attributes.
+    function delta(name, value) {
+        // An avenue of change happens to the sprite.
+    }
 }
 
 function instantiate() {
