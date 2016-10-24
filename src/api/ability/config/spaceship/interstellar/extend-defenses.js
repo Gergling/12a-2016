a109b7568814e5dfb1f5e9bd0277cf3b17baa59f
Greg <gregmichaeldavies@gmail.com>
2016-10-24 15:39:22 +0100

    Create extend-defenses.js

---
+++ src/api/ability/config/spaceship/interstellar/extend-defenses.js
@@ -0,0 +1,26 @@
+module.exports = {
+    label: 'Extend Defenses',
+    range: {
+        minimum: 1,
+        maximum: 1
+    },
+    cooldown: 0,
+    toggle: true,
+    cost: {
+        energy: 1
+    },
+    effect: {
+        start: function () {
+            var numeric = require('../../../common/numeric');
+            var caster = this.caster().sprite();
+            [
+                'ams',
+                'shield'
+            ].forEach(function (defense) {
+                //var spare = caster.capacitor(defense).maximum().multiply(0.5);
+                //numeric.multiply(this.target().sprite().capacitor(defense).maximum)
+                // Need a way for maximum to half and for the target defense maximum to double.
+            }.bind(this));
+        }
+    }
+};
