5c5451708156c46584347081d95ca37b507601d3
Greg <gregmichaeldavies@gmail.com>
2016-10-24 12:47:19 +0100

    Update config.js

--- src/api/ability/factory/config.js
+++ src/api/ability/factory/config.js
@@ -7,8 +7,10 @@ function Ability() {
             maximum: 0
         },
         cooldown: 0, // The number of turns which goes by before the ability becomes available again.
-        cost: {} // This is a pairing of capacitor name against value.
-    }
+        area: 0, // Area affected by ability. Would probably be a mixed value.
+        cost: {}, // This is a pairing of capacitor name against value.
+        effect: function effect() {}
+    };
     
     // Effect might be too complicated to boil down to an object except for API purposes.
 }
