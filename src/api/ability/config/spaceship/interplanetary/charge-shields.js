460a45eaeec41bbc46cdd229958e3d9d8c5681b4
Greg <gregmichaeldavies@gmail.com>
2016-10-25 16:32:47 +0100

    Create charge-shields.js

---
+++ src/api/ability/config/spaceship/interplanetary/charge-shields.js
@@ -0,0 +1,28 @@
+module.exports = {
+    label: 'Charge Shields',
+    cost: {
+        // This needs to calculate in all related skills and equipment.
+        // Energy reduction calculations in terms of skills are a problem.
+        // Maybe energy cost should be static and shield recharge should be determined by skill level.
+        energy: {
+            skill: {
+                'science.technology.engineering.defenses.shields': '-'
+            },
+            // Will have to name various bits of equipment.
+            equipment: 'defense.shield'
+        }
+    },
+    effect: function () {
+        // Effect is simply to charge the shield based on the skills of the operator.
+        var skills = [
+            'science.technology.engineering.defenses.shields',
+        ].map(function (name) {
+            return this.character().skill(name);
+        });
+
+        // Increase to shields requires a skill, buff and equipment bonus.
+        this.caster().sprite().capacitor('shield').delta(skill([
+            ''
+        ]));
+    }
+};
