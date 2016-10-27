56d3eba5fb3c70a1cfea73763434164940ec5195
Greg <gregmichaeldavies@gmail.com>
2016-10-27 17:31:42 +0100

    Update config.js

--- src/api/ability/factory/config.js
+++ src/api/ability/factory/config.js
@@ -27,7 +27,7 @@ function Ability() {
     function canCast() {
         // Check if capacitors contain sufficient value.
     }
-    function cast() {
+    function cast(caster) {
         // When the effect occurs, all attributes need to be calculated, including skill-affected attributes.
         Object.keys(data.cost).forEach(function (capacitorName) {
             // Find costs.
@@ -41,6 +41,11 @@ function Ability() {
                 Object.keys(weight[prop]).forEach(function (reference) {
                     //weight[prop][reference]
                     // Use weight against skill level to reduce cost of ability somehow.
+                    // Getting the skill level for the user would be caster.skill(reference).
+                    // Getting the equipment would be caster.equipment(reference).
+                    // Getting the buff would probably be complicated... caster.buff(reference)?
+                    // That implies you can apply a buff to a caster and it applies here somehow... 
+                    // maybe ability.[abilityName] if it's applied to the caster.
                 });
                 // Reduction could be 1 - skill or 1 / skill.
                 
