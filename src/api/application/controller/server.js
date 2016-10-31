ad4cfe5c17225225d6a6b1ce189ed7228355381a
gergling <gregmichaeldavies@gmail.com>
2016-10-31 18:02:07 +0000

    Included application routes.

--- src/api/application/controller/server.js
+++ src/api/application/controller/server.js
@@ -5,6 +5,8 @@ var port = process.env.PORT || 8080;
 app.use(express.static('./src/client/'));
 app.use(express.static('./dist/'));
 
+require('./routes.js')(app);
+
 app.listen(port);
 
 module.exports = app;
