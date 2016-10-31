6c7b12ac698c68361f7ef4b88f9901b8ec4bbc41
gergling <gregmichaeldavies@gmail.com>
2016-10-31 17:52:23 +0000

    Somehow responds to changes in code and restarts the server. Some investigation should be involved.

--- grunt/express.js
+++ grunt/express.js
@@ -1,9 +1,19 @@
+var path = require('path');
+
 module.exports = function express(grunt) {
     return {
         server: {
             options: {
                 script: 'src/api/application/controller/server.js',
                 port: 8080
+            },
+            livereload: {
+                options: {
+                    server: path.resolve('./app.js'),
+                    livereload: true,
+                    serverreload: true,
+                    bases: [path.resolve('./public')]
+                }
             }
         }
     };
