3fef0ad9fdfacb51737b5257b2d04154dd246d89
gergling <gregmichaeldavies@gmail.com>
2016-10-31 21:40:42 +0000

    Corrected order of loading for angular/jquery.

--- grunt/concat.js
+++ grunt/concat.js
@@ -5,8 +5,8 @@ module.exports = function concat(grunt) {
         },
         js: {
             src: [
-                'node_modules/angular/angular.js',
                 'node_modules/jquery/dist/jquery.js',
+                'node_modules/angular/angular.js',
                 'node_modules/bootstrap/dist/js/bootstrap.js',
                 'node_modules/angular-ui-router/release/angular-ui-router.js',
                 'node_modules/angular-animate/angular-animate.js',
