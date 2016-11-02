4a66883687dc13c70481742268fc80456491ba22
gergling <gregmichaeldavies@gmail.com>
2016-11-02 15:14:53 +0000

    Fixed manager to handle battle response correctly.

--- src/client/specific/battle/service/manager.js
+++ src/client/specific/battle/service/manager.js
@@ -1,8 +1,8 @@
 angular.module('battle').service('battleServiceManager', function (commonIsometricServiceTiles, battleServiceAPI) {
     function fetch() {
         return battleServiceAPI.get().then(function (response) {
-            response.map.tiles.forEach(function (tile) {
-                commonIsometricServiceTiles.tiles()
+            response.tiles.forEach(function (tile) {
+                commonIsometricServiceTiles.tile(tile.location.x, 0, tile.location.y);
             });
         });
     }
