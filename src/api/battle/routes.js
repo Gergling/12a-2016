cef9ba7514dbcd9947bf6c5798b5f5548a0cf061
gergling <gregmichaeldavies@gmail.com>
2016-11-02 15:12:21 +0000

    Hardcoded a backend output for a battle scenario.

--- src/api/battle/routes.js
+++ src/api/battle/routes.js
@@ -1,8 +1,40 @@
 module.exports = function (api) {
+    var tiles = [];
+
+    // Temporary tile generator
+    var x = 0, z = 0;
+    for (z = 0; z < 4; z += 1) {
+        for (x = 0; x < 4; x += 1) {
+            tiles.push({
+                name: 'empty',
+                location: {
+                    x: x,
+                    y: z
+                }
+            });
+        }
+    }
+
     api.get('/battle', function (req, res) {
         res.send({
-            map: {},
-            sprites: []
+            scale: 'spaceship',
+            name: 'interplanetary',
+            tiles: tiles,
+            sprites: [
+                {
+                    name: 'vessel',
+                    location: {
+                        x: 0,
+                        y: 0
+                    },
+                    abilities: ['dostuff']
+                }
+            ],
+            abilities: [
+                {
+                    name: 'dostuff'
+                }
+            ]
         });
     });
 };
