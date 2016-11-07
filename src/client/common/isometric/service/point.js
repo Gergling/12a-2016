38ab6a5d10e564c6a6310a74b45d1412ff7e8f0a
gergling <gregmichaeldavies@gmail.com>
2016-11-07 18:23:24 +0000

    Removed superfluous code.

--- src/client/common/isometric/service/point.js
+++ src/client/common/isometric/service/point.js
@@ -5,34 +5,6 @@ angular.module('commonIsometric').service("isometric.service.point", function ()
             y: 0,
             z: 0
         };
-        // x = x || 0;
-        // y = y || 0;
-        // z = z || 0;
-        // this.set = function (a, b, c) {
-        //     x = a;
-        //     y = b;
-        //     z = c;
-        // };
-        // this.x = function (value) {
-        //     if (value === 0 || value) {x = value; }
-        //     return x;
-        // };
-        // this.y = function (value) {
-        //     if (value === 0 || value) {y = value; }
-        //     return y;
-        // };
-        // this.z = function (value) {
-        //     if (value === 0 || value) {z = value; }
-        //     return z;
-        // };
-        // this.add = function (a, b, c) {
-        //     if (typeof a === "number") {
-        //         // Assume this is a change to the co-ordinates.
-        //         x += a || 0;
-        //         y += b || 0;
-        //         z += c || 0;
-        //     }
-        // };
 
         function apply(fnc) {
             Object.keys(data).forEach(fnc);
@@ -48,14 +20,12 @@ angular.module('commonIsometric').service("isometric.service.point", function ()
         }));
 
         function set(a, b, c) {
-            console.warn('Deprecate this function.')
             data.x = a;
             data.y = b;
             data.z = c;
         }
         function add(a, b, c) {
             if (typeof a === "number") {
-                console.warn('Deprecate this usage.');
                 // Assume this is a change to the co-ordinates.
                 data.x += a || 0;
                 data.y += b || 0;
