69fe478de13de1a7f589ee32d7be274fa4759be9
gergling <gregmichaeldavies@gmail.com>
2016-11-17 18:23:49 +0000

    Map service.

---
+++ src/api/map/service.js
@@ -0,0 +1,17 @@
+var data = {
+    tiles: []
+};
+
+function tile(location) {
+    var filtered = tiles().filter(function (tile) {
+        return tile.location().x() === location.x() && tile.location().y() === location.y();
+    });
+    if (!filtered) {
+        throw new Error('map/service: No tile at location (' + location.x() + ', ' + location.y() + ').');
+    }
+    return filtered[0];
+}
+
+module.exports = {
+    tile: tile
+};
