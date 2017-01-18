30ac03235346e85e05b20750892dce974e5f8921
gergling <gregmichaeldavies@gmail.com>
2017-01-18 12:43:35 +0000

    Task config constructor fixes.

--- src/api/task/constructor/config.js
+++ src/api/task/constructor/config.js
@@ -1,24 +1,35 @@
 var spriteFactory = require('../../sprite/factory/db');
 
 function SpriteGenerator(data, taskConfig) {
-    function generate() {
-        return spriteFactory(data.location, taskConfig, data.name);
+    function generate(location) {
+        // Also need to pass in other behavioural factors.
+        // E.g. pirate sprites will board, destroyer bots will destroy, and mysteries will spawn different sprites on
+        // death.
+        return spriteFactory(location || data.location, taskConfig, data.name);
+    }
+
+    this.generate = generate;
+}
+
+function TileGenerator(data, taskConfig) {
+    function generate(location) {
+        return tileFactory(location, taskConfig, data.name);
     }
 }
 
 function TaskConfig() {
     // Terrain should be generated according to player sprite visibility.
     // Sprites should be generated... somehow.
-    var generators = {
-    };
+    var generator = {};
 
     function initialise(value) {
+        var success = true;
         if (typeof value.tiles === 'string') {
 
         }
         if (value.sprites !== undefined) {
             if (value.sprites.constructor === [].constructor) {
-                generators.sprites = value.sprites.map(function (spriteData) {
+                generator.sprites = value.sprites.map(function (spriteData) {
                     // return a sprite generator
                     return new SpriteGenerator(spriteData, this);
                 }.bind(this));
@@ -27,11 +38,18 @@ function TaskConfig() {
                     'task/constructor/config: Task configuration failure, \'sprites\' property is not an array.'
                 ].join(''));
             }
+        } else {
+            generator.sprites = [];
         }
+        return success;
+    }
 
+    function generators(prop) {
+        return generator[prop];
     }
 
     this.initialise = initialise;
+    this.generators = generators;
 }
 
 module.exports = TaskConfig;
