6b31075ec6022c419dfbf576f12e1a29327821ad
gergling <gregmichaeldavies@gmail.com>
2017-01-18 00:01:34 +0000

    Context service load.

--- src/api/application/controller/server.js
+++ src/api/application/controller/server.js
@@ -2,6 +2,17 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
+require('../db');
+
+// Todo: Start everything else when the db connection succeeds.
+// Otherwise fail with message.
+
+require('../../context/service')
+    .load()
+    .catch(function (error) {
+        throw error;
+    });
+
 app.use(express.static('./src/client/'));
 app.use(express.static('./dist/'));
 
