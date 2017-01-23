f7ce76e93aba5b6e3b42ad3ca1d5f5da496e196d
Greg <gregmichaeldavies@gmail.com>
2017-01-23 17:01:04 +0000

    Create db.js

---
+++ src/api/attribute/service/db.js
@@ -0,0 +1,11 @@
+var dbFactory = require('./factory/db');
+
+function load() {
+    // Load the data.
+    // Start mapping nodes in from the root.
+    // Return a promise.
+}
+
+module.exports = {
+    load: load
+};
