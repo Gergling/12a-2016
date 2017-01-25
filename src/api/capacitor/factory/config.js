d15016acf3d00667e22f28b161a42d020e1082e6
Greg <gregmichaeldavies@gmail.com>
2017-01-25 17:19:21 +0000

    Update config.js

--- src/api/capacitor/factory/config.js
+++ src/api/capacitor/factory/config.js
@@ -1,14 +1,50 @@
+// Todo:
+// For one thing, think about whether this will have a config/db split.
+// This should go through 3 phases: proposing the change (propose()),
+    // checking the validity (valid()), and attempting the change (run()).
+// If the capacitor is setup to allow the change, or has sufficient current vs capacity to make the change, it runs.
+// If not, it doesn't.
+// Find whether this overflows the boundaries with overflow()
+// Reset the proposal with reset()
+// Allow the capacitor to contain the change with contain('none|both(default)|maximum|minimum')
+
 function Capacitor() {
     var data = {
         maximum: 1,
         current: 0,
-        can: {
-            overflow: false,
-            underflow: false
+        overflow: {
+            maximum: false,
+            minimum: false
         },
+        proposal: 0,
         attribute: require('../../attribute/factory')()
     };
 
+    function propose(value) {
+        data.proposal = value;
+        return this;
+    }
+    function reset() {
+        data.proposal = 0;
+        return this;
+    }
+    function delta() {
+        return data.current + data.proposal;
+    }
+    function overflow() {
+        var d = delta();
+        // Todo: Almost certainly wrong. Fix.
+        return (d > data.maximum && data.overflow.maximum) || (d < data.minimum && data.overflow.minimum);
+    }
+    function valid() {
+        // Return whether proposal is within boundaries, assuming overflow is not permitted.
+    }
+    function run() {
+        data.current += delta();
+        reset();
+        return this;
+    }
+    
     function current(value, setMode) {
         if (value) {
             if (setMode) {
